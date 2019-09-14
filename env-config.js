2b0b6c51f896c2d96b52f208ad6f2cd6cf07dc98
Andrew Rhee <adrhee@gmail.com>
2019-09-13 20:59:23 -0700

    fix uri

--- env-config.js
+++ env-config.js
@@ -2,8 +2,8 @@ const prod = process.env.NODE_ENV === "production";
 
 module.exports = {
   "process.env.BASE_URL": prod
-    ? "https://portfolio-rhee.herokuapp.com"
+    ? "http://www.andrewrhee.me"
     : "http://localhost:3000",
-  "process.env.NAMESPACE": "https://portfolio-rhee.herokuapp.com",
+  "process.env.NAMESPACE": "http://www.andrewrhee.me",
   "process.env.CLIENT_ID": "WrH4Ekk63hsiOGvc7hruEuH6p8RgLaxX"
 };
