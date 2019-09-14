8ea37eda81881bea937d64710c9973474a595eaa
Andrew Rhee <adrhee@gmail.com>
2019-09-14 16:41:58 -0700

    Change namespace

--- env-config.js
+++ env-config.js
@@ -2,8 +2,8 @@ const prod = process.env.NODE_ENV === "production";
 
 module.exports = {
   "process.env.BASE_URL": prod
-    ? "http://www.andrewrhee.me"
+    ? "https://www.andrewrhee.me"
     : "http://localhost:3000",
-  "process.env.NAMESPACE": "http://www.andrewrhee.me",
+  "process.env.NAMESPACE": "https://www.andrewrhee.me",
   "process.env.CLIENT_ID": "WrH4Ekk63hsiOGvc7hruEuH6p8RgLaxX"
 };
--- server/config/prod.js
+++ server/config/prod.js
@@ -1,5 +1,5 @@
 module.exports = {
   DB_URI:
     "mongodb://admin:admin@cluster0-shard-00-00-a0zqa.mongodb.net:27017,cluster0-shard-00-01-a0zqa.mongodb.net:27017,cluster0-shard-00-02-a0zqa.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority",
-  NAMESPACE: "http://www.andrewrhee.me"
+  NAMESPACE: "https://www.andrewrhee.me"
 };
